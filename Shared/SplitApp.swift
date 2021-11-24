849e405070554b1713725593bdfb466aa7fbdb11
Adam Patyk <apatyk@g.clemson.edu>
2021-11-24 16:52:34 -0500

    Initial Commit

---
+++ Shared/SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitApp.swift
+//  Shared
+//
+//  Created by Adam Patyk on 11/24/21.
+//
+
+import SwiftUI
+
+@main
+struct SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
