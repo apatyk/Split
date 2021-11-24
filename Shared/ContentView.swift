a1e4f084d1b29a11120366f8db4c4b4a28cd0fd0
Adam Patyk <apatyk@g.clemson.edu>
2021-11-24 16:53:36 -0500

    Initial commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    // PROPERTIES
+    
+    // BODY
     var body: some View {
         Text("Hello, world!")
-            .padding()
     }
 }
 
+
+// PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
